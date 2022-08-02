12d2f57b486cf99f5d8170c51cee06221c69eda3
lusida0131 <ks12b9189@naver.com>
2022-08-02 15:51:09 +0900

    책 서비스 책 등록하기 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,7 +1,32 @@
 package site.metacoding.junitproject.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록하기
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+    // 2. 책 목록보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 삭제
+
+    // 5. 책 수정
+
 }
